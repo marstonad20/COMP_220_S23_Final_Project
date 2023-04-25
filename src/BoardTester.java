8f0d2b675154de0e64f5b6b7516e4e1d5cfd519d
BROWNNJ20 <brownnj20@gcc.edu>
2023-04-25 19:38:58 -0400

    Amended ascii presentation of printed board

---
+++ src/BoardTester.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class BoardTester {
+
+    public static void main(String[] args) throws Exception {
+
+        Board testBoard = new Board();
+        testBoard.printBoard();
+
+    }
+
+}
